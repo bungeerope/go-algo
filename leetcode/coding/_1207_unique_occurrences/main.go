cb9fc39522dd1328785e8e123fe755985a7eddef
ZhihaoQuinn <howzequinn@gmail.com>
2020-10-28 22:12:56 +0800

    feat(leet code): 1207_uniqueOccurrences
    1207_uniqueOccurrences

---
+++ leetcode/coding/_1207_unique_occurrences/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func uniqueOccurrences(arr []int) bool {
+	countMap := make(map[int]int)
+
+	for _, ele := range arr {
+		countMap[ele]++
+	}
+	times := make(map[int]int)
+	for _, c := range countMap {
+		times[c]++
+	}
+
+	return len(times) == len(countMap)
+}
+
+func main() {
+	fmt.Println(uniqueOccurrences([]int{1, 2, 2, 1, 1, 3}))
+	fmt.Println(uniqueOccurrences([]int{1, 2}))
+	fmt.Println(uniqueOccurrences([]int{-3, 0, 1, -3, 1, 1, 1, -3, 10, 0}))
+}
